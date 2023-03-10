beafedeb398cb8a440571c0c3d4e7b8585d75be3
Oleg-Pashchenko <pickpar@ya.ru>
2023-03-10 23:01:11 +0300

    wa 11 test

--- TaskC.cpp
+++ TaskC.cpp
@@ -4,47 +4,58 @@
 
 using namespace std;
 
-unordered_map <string, int> variables;
-stack <unordered_map<string, int>> order;
+
+unordered_map<string, int> variables;
+stack<unordered_map<string, int>> operationsHistory;
 
 
-int getValue(const string& key) {
+int getValue(const string &key) {
     if (variables.find(key) == variables.end()) variables[key] = 0;
     return variables[key];
 }
 
-
-bool isInt(const string& possibleInt) {
-    bool result;
+bool isInt(const string &possibleInt) {
     try {
         stoi(possibleInt);
-        result = true;
-    } catch (invalid_argument& e) {
-        result = false;
+        return true;
+    } catch (invalid_argument &e) {
+        return false;
     }
-    return result;
+}
+
+bool isMapContainsKey(const unordered_map<string, int> &map, const string &key) {
+    return (map.count(key) != 0);
 }
 
 
 int main() {
     string line, key, value;
+    operationsHistory.emplace();
+
     while (getline(cin, line)) {
-        if (line == "{") {
-            order.push(variables);
-        }
+
+        if (line == "{") operationsHistory.emplace();
         else if (line == "}") {
-            variables = order.top();
-            order.pop();
-        }
-        else {
+            for (auto &[k, v]: operationsHistory.top()) {
+                variables[k] += operationsHistory.top()[k];
+            }
+            operationsHistory.pop();
+        } else {
             key = line.substr(0, line.find('='));
             value = line.substr(line.find('=') + 1);
+
+            if (!isMapContainsKey(operationsHistory.top(), key)) {
+                operationsHistory.top()[key] = variables[key];
+            }
+
             if (isInt(value)) {
                 variables[key] = stoi(value);
             } else {
                 variables[key] = getValue(value);
                 cout << variables[key] << endl;
             }
+
+            operationsHistory.top()[key] -= variables[key];
         }
     }
     return 0;
